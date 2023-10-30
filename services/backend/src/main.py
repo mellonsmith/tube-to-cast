6b7ea8b744ccbcfce2766422c14c6429379c89f6
mellonsmith <mellon385champi@gmail.com>
2023-10-30 02:01:25 +0100

    added apis for feeds and videos

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
-from fastapi.middleware.cors import CORSMiddleware  # NEW
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
 from .database import SessionLocal, engine, Base
 from .models import Video
@@ -9,7 +10,6 @@ import yt_dlp
 import pytz
 import podgen
 import datetime
-
 
 app = FastAPI()
 
@@ -22,8 +22,9 @@ app.add_middleware(
 )
 
 # Dependency to get database session
-
 Base.metadata.create_all(bind=engine)
+
+base_url = "http://localhost:5000"
 
 
 def get_db():
@@ -68,6 +69,16 @@ def get_videos(db: Session = Depends(get_db)):
     return videos
 
 
+@app.get("/videos/{video_id}")
+def get_video(video_id: int, db: Session = Depends(get_db)):
+    video = db.query(Video).filter(Video.id == video_id).first()
+
+    if not video:
+        raise HTTPException(status_code=404, detail="Video not found")
+
+    return FileResponse(video.file_path, media_type="video/mp4")
+
+
 @app.delete("/videos/{video_id}")
 def del_video(video_id: int, db: Session = Depends(get_db)):
     # Delete video from database
@@ -110,13 +121,20 @@ def generate_podcast(db: Session = Depends(get_db)):
         episode.publication_date = video.date.astimezone(timezone)
 
         episode.media = podgen.Media(
-            video.file_path, type="video/mp4", size=video.filesize)
+            f"{base_url}/videos/{video.id}", type="video/mp4", size=video.filesize)
 
         episode.length = video.filesize
         episode.image = video.thumbnail_url
 
     # Generate the RSS feed and return it as a string
-    with open("feed.rss", "w") as f:
+    with open("public/feed.rss", "w") as f:
         f.write(podcast.rss_str())
 
     return "Podcast updated"
+
+
+@app.get("/feed.rss")
+async def serve_rss_feed():
+    rss_feed_path = "public/feed.rss"
+
+    return FileResponse(rss_feed_path, media_type="application/rss+xml")
